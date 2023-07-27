e4e2fe780fbd6889acfabf929418558c3e6e2c16
Deshan <dcnm777@gmail.com>
2023-07-27 15:50:28 -0700

    package.json and server.js updates

--- Develop/server.js
+++ Develop/server.js
@@ -1,57 +1,67 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET route for notes page
+// GET route for notes.html
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET route for retrieving all the notes
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/db/db.json'));
-});
-
-// POST route for a new note
-app.post('/api/notes', (req, res) => {
-  // read note data from json file
-  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-
-  // create a new note object
-  let newNote = {
-    title: req.body.title,
-    text: req.body.text,
-    id: uuidv4(), // create unique id for the note
-  };
-
-  // push newNote into notes array
-  notes.push(newNote);
-
-  // write new notes array to json file
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-  
-  res.json(notes);
-});
-
-// * route
+// GET route for index.html
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET route for retrieving all the notes
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read notes' });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// POST route for a new note
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(), // Assigning a unique ID to the note
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to read notes' });
+      }
+      const allNotes = JSON.parse(data);
+      allNotes.push(newNote);
+      
+      fs.writeFile('./db/db.json', JSON.stringify(allNotes, null, 4), (writeErr) =>
+        writeErr
+          ? console.error(writeErr)
+          : console.info('Successfully added new note!')
+      );
+    });
+    res.json(newNote);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
